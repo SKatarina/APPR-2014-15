f506606a6dac67fec2a053320bcce152e5cf4452
Katarina Šutar <SutarK12@R309-08.std.fmf.uni-lj.si>
2014-12-03 12:32:13 +0100

    Uvozila sem tabele in narisala grafe.

---
+++ slike/M_zmage.R
@@ -0,0 +1,25 @@
+pdf("slike/Mzmage.pdf") #naslov pdfja
+pie(Mzmage[,"Zmage"], labels = gsub(" ", "\n", Mzmage[,1]))
+text(0,1,"Posamične zmage za moške", cex =2)
+dev.off()
+
+pdf("slike/Zzmage.pdf") #naslov pdfja
+pie(Zzmage[,"Zmage"], labels = Zzmage[,1])
+text(0,1,"Posamične zmage za ženske", cex =2)
+dev.off()
+
+
+pdf("slike/pokali.pdf") #naslov pdfja
+pie(table(pokali[,"Prva"]), main = "Največkrat osvojeno\n1. mesto po državah", cex.main = 1.5)
+#text(0,1,"Največkrat osvojeno\n1. mesto po državah", cex =2)
+dev.off()
+
+pdf("slike/pokali2.pdf") #naslov pdfja
+pie(table(pokali[,"Druga"]), main = "Največkrat osvojeno\n2. mesto po državah", cex.main = 1.5)
+#text(0,1,"Največkrat osvojeno\n2. mesto po državah", cex =2)
+dev.off()
+
+pdf("slike/pokali3.pdf") #naslov pdfja
+pie(table(pokali[,"Tretja"]), main = "Največkrat osvojeno\n3. mesto po državah", cex.main = 1.5)
+#text(0,1,"Največkrat osvojeno\n3. mesto po državah", cex =2)
+dev.off()
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,82 @@
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+uvozirekordi <- function() {
+  return(read.table("podatki/rekordi.csv", sep = ",", as.is = TRUE,
+                     header = TRUE, fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+# Zapišimo podatke v razpredelnico najvišji zaslužek v sezoni za m.
+cat("Uvažam podatke o rekordih za moske...\n")
+rekordi <- uvozirekordi()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+
+uvozipokali <- function() {
+  return(read.table("podatki/pokali.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+  cat("Uvažam podatke o pokalih narodov ...\n")
+  pokali <- uvozipokali()
+  
+
+uvoziekipnezmage <- function() {
+  return(read.table("podatki/ekipnezmage.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o ekipnih zmagah za moske ...\n")
+ekipnezmage <- uvoziekipnezmage()
+
+uvozimaxzasluzekM <- function() {
+  return(read.table("podatki/maxzasluzekM.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o največjih zasluzkih v sezoni za moske ...\n")
+maxzasluzekM <- uvozimaxzasluzekM()
+
+uvozimaxzasluzekZ <- function() {
+  return(read.table("podatki/maxzasluzekZ.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o največjih zasluzkih v sezoni za zenske ...\n")
+maxzasluzekZ <- uvozimaxzasluzekZ()
+
+uvoziMzmage <- function() {
+  return(read.table("podatki/Mzmage.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o zmagah za moske ...\n")
+Mzmage <- uvoziMzmage()
+
+uvoziZzmage <- function() {
+  return(read.table("podatki/Zzmage.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o zmagah za zenske ...\n")
+Zzmage <- uvoziZzmage()
+
+uvozim.k.globus <- function() {
+  return(read.table("podatki/m.k.globus.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o tekmovalcih, ki so osvojili mali kristalni globus ...\n")
+m.k.globus <- uvozim.k.globus()
+
+uvoziv.k.globus <- function() {
+  return(read.table("podatki/v.k.globus.csv", sep = ",", as.is = TRUE,
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+cat("Uvažam podatke o tekmovalcih, ki so osvojili veliki kristalni globus ...\n")
+v.k.globus <- uvoziv.k.globus()
