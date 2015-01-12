d5c2b30efd62da6b450f89c5e47cf683b85fa3d0
Katarina Šutar <SutarK12@R308-02.std.fmf.uni-lj.si>
2015-01-12 11:15:25 +0100

    Zmanjšana velikost pisave pri enem grafu.

--- slike/M_zmage.R
+++ slike/M_zmage.R
@@ -4,10 +4,9 @@ text(0,1,"Posamične zmage za moške", cex =2)
 dev.off()
 
 pdf("slike/Zzmage.pdf") #naslov pdfja
-pie(Zzmage[,"Zmage"], labels = Zzmage[,1])
+pie(Zzmage[,"Zmage"], labels = Zzmage[,1], init.angle=330, cex=0.8)
 text(0,1,"Posamične zmage za ženske", cex =2)
 dev.off()
-
 
 pdf("slike/pokali.pdf") #naslov pdfja
 pie(table(pokali[,"Prva"]), main = "Največkrat osvojeno\n1. mesto po državah (2000-2014)", cex.main = 1.5)
