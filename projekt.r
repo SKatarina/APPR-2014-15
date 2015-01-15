853a767131408dcefa622f760fbc375b0e1feb97
Katarina Šutar <SutarK12@R309-06.std.fmf.uni-lj.si>
2015-01-15 13:53:58 +0100

    Odkomentirana vizualizacija.

--- projekt.r
+++ projekt.r
@@ -1,11 +1,11 @@
 # 2. faza: Obdelava, uvoz in čiščenje podatkov
-source("uvoz/uvoz.r")
-source("slike/M_zmage.R")
+source("uvoz/uvoz.r", encoding="UTF-8")
+source("slike/M_zmage.R", encoding="UTF-8")
 
 # 3. faza: Analiza in vizualizacija podatkov
-#source("vizualizacija/vizualizacija.r")
+source("vizualizacija/vizualizacija.r", encoding="UTF-8")
 
 # 4. faza: Napredna analiza podatkov
-#source("analiza/analiza.r")
+source("analiza/analiza.r")
 
 cat("Končano.\n")
